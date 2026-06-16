b8bcd0e676a26b9bf053f116f9dafdda79a3c5f9
DenisChavez <dabemois2004@gmail.com>
2026-06-15 23:20:08 -0600

    fix: resolución móvil terminado

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -2,8 +2,8 @@
 export const Footer = () => {
     return (
         <section className="footer">
-            <p>creatividad y diseño de landing por: <strong>hellocuantica</strong>.</p>
-            <p>Todos los derechos reservados para la <strong>jardinera.life®,</strong>Honduras C.A.</p>
+            <p>creatividad y diseño de landing <strong>hellocuantica</strong>.</p>
+            <p>todos los derechos reservados para la <strong>jardinera.life®,</strong>Honduras C.A.</p>
         </section>
     )
 };
